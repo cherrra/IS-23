66923a78d6d6cd52c8154932824e1fd19c4356a4
cherrra <annna.cherrr@yandex.ru>
2023-01-28 18:32:45 +0300

    commit

--- Proj_1sem_Cherevko/PZ_12/PZ_12_2.py
+++ Proj_1sem_Cherevko/PZ_12/PZ_12_2.py
@@ -5,5 +5,3 @@ lst = ['Оттава', 'Москва', 'Пекин', 'Полоцк', 'Верса
 vse_sogl = ['б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ']
 sogl = [k.upper() for i in range(len(lst)) for k in lst[i] if k.lower() in vse_sogl]
 print(sogl)
-
-
