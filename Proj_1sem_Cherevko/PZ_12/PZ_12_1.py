390c4348f389dae35b425c908b53d0990264de2f
cherrra <annna.cherrr@yandex.ru>
2023-01-28 16:21:06 +0300

    commit

--- Proj_1sem_Cherevko/PZ_12/PZ_12_1.py
+++ Proj_1sem_Cherevko/PZ_12/PZ_12_1.py
@@ -6,5 +6,5 @@ lst = [randint(150, 231) for i in range(20)]
 basket = [i for i in lst if i >= 190]
 football = [i for i in lst if i < 190]
 print(lst)
-print('Кол-во юношей, попавших в баскетбольную команду:', len(basket))
-print('Кол-во юношей, попавших в футбольную команду:', len(football))
+print('Количество юношей, попавших в баскетбольную команду:', len(basket))
+print('Количество юношей, попавших в футбольную команду:', len(football))
